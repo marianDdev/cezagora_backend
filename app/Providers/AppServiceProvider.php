48c27221930d89a2777331482084a940b6fb3ae9
Marian Dumitru <d.mariandev@gmail.com>
2022-11-27 23:27:47 +0200

    disable primary key

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\MigrationsEnded;
+use Illuminate\Database\Events\MigrationsStarted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +17,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Event::listen(MigrationsStarted::class, function (){
+            if (config('databases.allow_disabled_pk')) {
+                DB::statement('SET SESSION sql_require_primary_key=0');
+            }
+        });
+
+        Event::listen(MigrationsEnded::class, function (){
+            if (config('databases.allow_disabled_pk')) {
+                DB::statement('SET SESSION sql_require_primary_key=1');
+            }
+        });
     }
 
     /**
