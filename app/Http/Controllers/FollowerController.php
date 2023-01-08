af578b67bddb8afa07342e726127abeb9616f57d
Marian Dumitru <d.mariandev@gmail.com>
2023-01-08 13:36:46 +0200

    unfollow

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -3,11 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Services\NetworkingService;
+use App\Services\OrganizationService;
+use Illuminate\Http\JsonResponse;
 
 class FollowerController extends Controller
 {
-    public function follow(NetworkingService $networkingService, int $followedOrganizationId)
+    public function follow(
+        OrganizationService $organizationService,
+        NetworkingService $networkingService,
+        int $followedOrganizationId
+    ): JsonResponse
     {
-        return $networkingService->follow($followedOrganizationId);
+        $authOrganizationid = $organizationService->getAuthOrganization()->id;
+        $existingFollow = $networkingService->getFollowing($followedOrganizationId);
+
+        if ($followedOrganizationId === $authOrganizationid) {
+            return response()->json('You are trying to follow your own company.');
+        }
+
+        if (!is_null($existingFollow)) {
+            return response()->json('You are already following this company.');
+        }
+
+
+        return response()->json('Successfully following.');
+    }
+
+    public function unFollow(NetworkingService $networkingService, int $followedOrganizationId): JsonResponse
+    {
+        $existingFollowing = $networkingService->getFollowing($followedOrganizationId);
+
+        if (is_null($existingFollowing)) {
+            return response()->json("This following doesn't exist.", 404);
+        }
+
+        $existingFollowing->delete();
+
+        return response()->json("Successfully unfollowed.");
     }
 }
